1bc4abe9bef74d4e4449a7c8302366d38c3b095c
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 <tusharpatil22700@gmail.com>
2022-11-30 13:59:02 -0800

    welcome

---
+++ src/main/java/bridgelabz/EmployeePayroll/App.java
@@ -0,0 +1,13 @@
+package bridgelabz.EmployeePayroll;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/bridgelabz/EmployeePayroll/JdbcEmpPayroll.java
@@ -0,0 +1,9 @@
+package bridgelabz.EmployeePayroll;
+
+public class JdbcEmpPayroll {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to employee payroll ");
+
+	}
+}
